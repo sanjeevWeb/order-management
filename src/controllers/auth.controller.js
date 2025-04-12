c69b0d226a32fea51e55c46a1712cbcce9f89838
sanjeev <kumarsanjeevdutta02@gmail.com>
2025-04-12 11:40:01 +0530

    write controller for auth

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -33,7 +33,7 @@ const login = async (req, res, next) => {
             return res.status(401).send({ message: "Wrong Credentials" })
         }
         const token = jwt.sign({ userId: user._id, role: user.role }, process.env.JWT_SECRET_KEY, { expiresIn: '1d' })
-        return res.status(200).send(token)
+        return res.status(200).send({token})
     }
     catch (error) {
         return res.status(500).send({ message: "Something broke, Try after sometime" })
