7aea2e3d046e0cce37198bad71cd9dc3be852cf6
sanjeev <kumarsanjeevdutta02@gmail.com>
2025-04-14 12:38:07 +0530

    added yr

--- src/controllers/vendor.controller.js
+++ src/controllers/vendor.controller.js
@@ -2,58 +2,70 @@ const productModel = require("../models/product.model")
 
 const addProduct = async (req, res, next) => {
     try {
-        const newProduct = await productModel.create({...req.body, vendorId: req.user._id})
+        const newProduct = await productModel.create({ ...req.body, vendorId: req.user._id })
         return res.status(201).send(newProduct)
-    } 
+    }
     catch (error) {
-        return res.status(500).send({ message: "Something broke, PLese try after sometime"})
+        return res.status(500).send({ message: "Something broke, PLese try after sometime" })
     }
 }
 
 const updateProduct = async (req, res, next) => {
     try {
-        const {id} = req.params
-        const isProductFound = await productModel.findOne({_id: id})
+        const { id } = req.params
+        const isProductFound = await productModel.findOne({ _id: id })
         // console.log(isProductFound);
         // console.log(req.user);
-        if(!isProductFound){
-            return res.status(404).send({message: "No product found"})
-            
+        if (!isProductFound) {
+            return res.status(404).send({ message: "No product found" })
+
         }
-        if(isProductFound.vendorId.toString() != req.user._id){
-            return res.status(401).send({message: "Unauthorized acccess"})
-            
+        if (isProductFound.vendorId.toString() != req.user._id) {
+            return res.status(401).send({ message: "Unauthorized acccess" })
+
         }
-        const newProduct = await productModel.findByIdAndUpdate({_id:id},{...req.body}, {new:true})
+        const newProduct = await productModel.findByIdAndUpdate({ _id: id }, { ...req.body }, { new: true })
         return res.status(200).send(newProduct)
-    } 
+    }
     catch (error) {
-        return res.status(500).send({ message: "Something broke, PLese try after sometime"})
+        return res.status(500).send({ message: "Something broke, PLese try after sometime" })
     }
 }
 const deleteProduct = async (req, res, next) => {
     try {
-        const {id} = req.params
-        const isProductFound = await productModel.findOne({_id: id})
-        if(!isProductFound){
-            return res.status(404).send({message: "No product found"})
-            
+        const { id } = req.params
+        const isProductFound = await productModel.findOne({ _id: id })
+        if (!isProductFound) {
+            return res.status(404).send({ message: "No product found" })
+
         }
-        if(isProductFound.vendorId.toString() != req.user._id){
-            return res.status(401).send({message: "Unauthorized acccess"})
-            
+        if (isProductFound.vendorId.toString() != req.user._id) {
+            return res.status(401).send({ message: "Unauthorized acccess" })
+
         }
-        const newProduct = await productModel.findByIdAndDelete({_id: id})
+        const newProduct = await productModel.findByIdAndDelete({ _id: id })
         // console.log('newproduct', newProduct);
-        return res.status(200).send({message: 'This product deleted successfully'})
-    } 
+        return res.status(200).send({ message: 'This product deleted successfully' })
+    }
     catch (error) {
-        return res.status(500).send({ message: "Something broke, PLese try after sometime"})
+        return res.status(500).send({ message: "Something broke, PLese try after sometime" })
+    }
+}
+
+const getAllProductsByVendor = async (req, res, next) => {
+    try {
+        const _id = req.user._id
+        const products = await productModel.find({ vendorId: _id })
+        return res.status(200).send(products)
+    }
+    catch (error) {
+        return res.status(500).send({ message: "Something broke, PLese try after sometime" })
     }
 }
 
 module.exports = {
     addProduct,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    getAllProductsByVendor
 }
--- src/routes/vendor.route.js
+++ src/routes/vendor.route.js
@@ -1,4 +1,4 @@
-const { addProduct, updateProduct, deleteProduct } = require('../controllers/vendor.controller')
+const { addProduct, updateProduct, deleteProduct, getAllProductsByVendor } = require('../controllers/vendor.controller')
 const { authorization } = require('../middlewares/auth.middleware')
 const { authorizeRoles } = require('../middlewares/role.middleware')
 const validateRequest = require('../middlewares/validate-request.middleware')
@@ -11,5 +11,7 @@ router.post('/product', validateRequest(createProductSchema), authorization, aut
 router.put('/product/:id', validateRequest(updateProductSchema), authorization, authorizeRoles('vendor'), updateProduct)
 
 router.delete('/product/:id', authorization, authorizeRoles('vendor'), deleteProduct)
+
+router.get('/product', authorization, authorizeRoles('vendor'), getAllProductsByVendor)
 
 module.exports = router
